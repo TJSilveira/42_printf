521ccd983e4ec9025f1a49de59aec5a7875e1b91
Tiago Silveira <tiagojesusilveira@gmail.com>
2025-06-17 14:59:34 +0200

    Add precision to the print_u.c function

--- src/ft_print_u.c
+++ src/ft_print_u.c
@@ -1,30 +1,23 @@
 #include "ft_printf.h"
 
-void	ft_put_unsignbr_fd(unsigned int n, int fd)
-{
-	char	ch;
-
-	if (n >= 10)
-		ft_putnbr_fd(n / 10, fd);
-	ch = n % 10 + '0';
-	write(fd, &ch, 1);
-}
-
 int	ft_print_u(unsigned int nbr, t_format *spec)
 {
-	int				counter;
+	int				counter_total;
+	int				counter_nbr;
 	unsigned int	nbr_temp;
 
-	counter = 0;
+	counter_total = 0;
 	nbr_temp = nbr;
 	while (nbr_temp > 9)
 	{
 		nbr_temp = nbr_temp / 10;
-		counter++;
+		counter_total++;
 	}
-	counter++;
-	counter += ft_print_width_left(counter, spec);
-	ft_put_unsignbr_fd (nbr, 1);
-	counter += ft_print_width_right(counter, spec);
-	return (counter);
+	counter_total++;
+	counter_nbr = counter_total;
+	counter_total += ft_print_width_left(counter_total, spec);
+	counter_total += ft_print_prec(counter_nbr, spec);
+	ft_putunsnbr_fd(nbr, 1);
+	counter_total += ft_print_width_right(counter_total, spec);
+	return (counter_total);
 }
